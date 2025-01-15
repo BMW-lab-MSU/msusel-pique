3e09d8e1637bf1be1663c076930083a2bb754c28
kaveen <kaveenliyanage@yahoo.com>
2025-01-15 10:04:11 -0700

    Saving the model and the data

--- src/main/java/pique/calibration/MLWeighter.java
+++ src/main/java/pique/calibration/MLWeighter.java
@@ -26,6 +26,7 @@
 package pique.calibration;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -39,16 +40,22 @@ import pique.calibration.WeightResult;
 import pique.model.ModelNode;
 import pique.model.QualityModel;
 import pique.utility.BigDecimalWithContext;
+import pique.evaluation.ProbabilityDensityFunctionUtilityFunction;
+
 
 import weka.core.Utils;
 import weka.classifiers.Classifier;
 import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
+import weka.core.converters.CSVSaver;
 import weka.classifiers.functions.LinearRegression;
 import weka.classifiers.functions.SimpleLinearRegression;
 import weka.classifiers.functions.Logistic;
 import weka.core.WekaPackageManager;
+import weka.filters.Filter;
+import weka.filters.unsupervised.instance.RemovePercentage;
+import weka.filters.supervised.instance.StratifiedRemoveFolds;
 
 import weka.core.Attribute;
 import weka.core.DenseInstance;
@@ -338,6 +345,8 @@ public class MLWeighter implements IWeighter{
      */
     private void mlWeights(Collection<ModelNode> nodes, Set<WeightResult> weights) throws Exception{
         BigDecimal[][] normMat = normalizeByColSum(manWeights);
+        ProbabilityDensityFunctionUtilityFunction probabilityDensityFunctionUtilityFunction;
+        probabilityDensityFunctionUtilityFunction = new ProbabilityDensityFunctionUtilityFunction();
 
         for (ModelNode node : nodes) {
             WeightResult weightResult = new WeightResult(node.getName());
@@ -360,7 +369,7 @@ public class MLWeighter implements IWeighter{
 
             node.getChildren().values().forEach(child ->{
                     System.out.println(child.getName());
-                    weightResult.setWeight(child.getName(), normMat[ArrayUtils.indexOf(pfNames, child.getName())][ArrayUtils.indexOf(qaNames, node.getName())]);
+//                    weightResult.setWeight(child.getName(), normMat[ArrayUtils.indexOf(pfNames, child.getName())][ArrayUtils.indexOf(qaNames, node.getName())]);
 
 
                     child.getChildren().values().forEach( grandChild ->{
@@ -370,9 +379,20 @@ public class MLWeighter implements IWeighter{
                             ref.n_measures += 1;
                             ref.atts.add(new Attribute(grandChild.getName()));
 
-                            ref.n_projects = grandChild.getThresholds().length;
+                            BigDecimal[] thresholds = grandChild.getThresholds();
 
-                            ref.arrayVals.add(grandChild.getThresholds());
+                            ref.n_projects = thresholds.length;
+
+                            BigDecimal[] scores = new BigDecimal[0];
+
+                            // Generating PDF scores for the thresholds
+                            for(int i=0; i<ref.n_projects; i++){
+                                BigDecimal inValue = thresholds[i];
+                                BigDecimal score = probabilityDensityFunctionUtilityFunction.utilityFunction(inValue, thresholds, false);
+                                scores = addToArray(scores, score);
+                            }
+
+                            ref.arrayVals.add(scores);
 
                         } else {
                             System.out.println("***** double present");
@@ -394,147 +414,220 @@ public class MLWeighter implements IWeighter{
                 ref.data.add(new DenseInstance(1.0, ref.vals));
             }
 
-            System.out.println(ref.data);
+//            System.out.println(ref.data);
+
+            // Saving the dataset
+            CSVSaver saver = new CSVSaver();
+            saver.setInstances(ref.data);
+            saver.setFile(new File("./src/test/out/ML/"+ node.getName()+"_measures.csv"));
+            saver.setDestination(new File("./src/test/out/ML/"+ node.getName()+"_measures.csv"));
+            saver.writeBatch();
+
 
             try {
-                testWeka(ref.data);
+                LinearRegression model = testWeka(ref.data);
+//                node.setThresholds(model);
+                // Saving the model
+                System.out.println("----------saving model---------------");
+                weka.core.SerializationHelper.write("./src/test/out/ML/"+node.getName()+"_lin.model",model);
+
+
             } catch (Exception e) {
                 e.printStackTrace();
             }
+
+
 
 //            weights.add(weightResult);
         }
     }
 
-    public static void testWeka(Instances dataset) throws Exception{
-        // load packages and MTJ/arpack libraries (if necessary)
-//        WekaPackageManager.loadPackages(false);
+    private static BigDecimal[] addToArray(BigDecimal[] array, BigDecimal newElement) {
+        BigDecimal[] newArray = Arrays.copyOf(array, array.length + 1);
+        newArray[newArray.length - 1] = newElement;
+        return newArray;
+    }
+
+    public static LinearRegression testWeka(Instances dataset) throws Exception{
         //Load Data set
 
-//        DataSource source = new DataSource("/media/kaveen/D/Java_ML/weka-3-8-6/data/regression-datasets/regression-datasets/housing.arff");
+//        DataSource source = new DataSource("/PWD/weka-3-8-6/data/regression-datasets/regression-datasets/housing.arff");
 //        Instances dataset = source.getDataSet();
         //set class index to the last attribute
         dataset.setClassIndex(dataset.numAttributes()-1);
+
+
+        // Train test divide
+
+//        RemovePercentage filter = new RemovePercentage();
+//
+//        String[] options = new  String[4];
+//
+//        options[0] = "-P";
+//        options[1] = "70.0F";
+//        options[2] = "-V";
+//        options[3] = "1";
+//
+//
+//        filter.setOptions(options);
+//        Instances train = Filter.useFilter(dataset, filter);
+//
+//        options[3] = "0";
+//        filter.setOptions(options);
+//        Instances test = Filter.useFilter(dataset, filter);
+
+
+// use StratifiedRemoveFolds to randomly split the data
+        StratifiedRemoveFolds filter = new StratifiedRemoveFolds();
+
+// set options for creating the subset of data
+        String[] options = new String[6];
+
+        options[0] = "-N";                 // indicate we want to set the number of folds
+        options[1] = Integer.toString(5);  // split the data into five random folds
+        options[2] = "-F";                 // indicate we want to select a specific fold
+        options[3] = Integer.toString(1);  // select the first fold
+        options[4] = "-S";                 // indicate we want to set the random seed
+        options[5] = Integer.toString(1);  // set the random seed to 1
+
+        filter.setOptions(options);        // set the filter options
+        filter.setInputFormat(dataset);       // prepare the filter for the data format
+        filter.setInvertSelection(false);  // do not invert the selection
+
+// apply filter for test data here
+        Instances test = Filter.useFilter(dataset, filter);
+        System.out.println("test");
+        System.out.println(test.size());
+
+//  prepare and apply filter for training data here
+        filter.setInvertSelection(true);     // invert the selection to get other data
+        Instances train = Filter.useFilter(dataset, filter);
+        System.out.println("train");
+        System.out.println(train.size());
 
         //Build model
 //        SimpleLinearRegression model = new SimpleLinearRegression();
         LinearRegression model = new LinearRegression();
 //        Logistic model = new Logistic();
-        model.buildClassifier(dataset);
+        model.buildClassifier(train);
         //output model
         System.out.println("LR FORMULA : "+model);
 
-        // Saving the model
-        System.out.println("----------saving model---------------");
-        weka.core.SerializationHelper.write("./lin.model",model);
+//        // Saving the model
+//        System.out.println("----------saving model---------------");
+//        weka.core.SerializationHelper.write("./lin.model",model);
+//
+//        // loading the model
+//        System.out.println("----------loading model---------------");
+//        Classifier loaded_model = (Classifier) weka.core.SerializationHelper.read("./lin.model");
+//
+//        // Now Predicting the cost
+//        Instance myHouse = test.lastInstance();
+//        double price = loaded_model.classifyInstance(myHouse);
+//        System.out.println("-------------------------");
+//        System.out.println(myHouse);
+//        System.out.println("PREDICTING THE score : "+price);
 
-        // loading the model
-        System.out.println("----------loading model---------------");
-        Classifier loaded_model = (Classifier) weka.core.SerializationHelper.read("./lin.model");
 
-        // Now Predicting the cost
-        Instance myHouse = dataset.lastInstance();
-        double price = loaded_model.classifyInstance(myHouse);
-        System.out.println("-------------------------");
-        System.out.println(myHouse);
-        System.out.println("PREDICTING THE PRICE : "+price);
+
+        return model;
     }
 
-    public static void attTest() throws Exception {
-        ArrayList<Attribute> atts;
-        ArrayList<Attribute> attsRel;
-        ArrayList<String>    attVals;
-        ArrayList<String>    attValsRel;
-        Instances            data;
-        Instances            dataRel;
-        double[]             vals;
-        double[]             valsRel;
-        int                  i;
-
-        // 1. set up attributes
-        atts = new ArrayList<Attribute>();
-        // - numeric
-        atts.add(new Attribute("att1"));
-        // - nominal
-        attVals = new ArrayList<String>();
-        for (i = 0; i < 5; i++)
-            attVals.add("val" + (i+1));
-        atts.add(new Attribute("att2", attVals));
-        // - string
-        atts.add(new Attribute("att3", (ArrayList<String>) null));
-        // - date
-        atts.add(new Attribute("att4", "yyyy-MM-dd"));
-        // - relational
-        attsRel = new ArrayList<Attribute>();
-        // -- numeric
-        attsRel.add(new Attribute("att5.1"));
-        // -- nominal
-        attValsRel = new ArrayList<String>();
-        for (i = 0; i < 5; i++)
-            attValsRel.add("val5." + (i+1));
-        attsRel.add(new Attribute("att5.2", attValsRel));
-        dataRel = new Instances("att5", attsRel, 0);
-        atts.add(new Attribute("att5", dataRel, 0));
-
-        // 2. create Instances object
-        data = new Instances("MyRelation", atts, 0);
-
-        // 3. fill with data
-        // first instance
-        vals = new double[data.numAttributes()];
-        // - numeric
-        vals[0] = Math.PI;
-        // - nominal
-        vals[1] = attVals.indexOf("val3");
-        // - string
-        vals[2] = data.attribute(2).addStringValue("This is a string!");
-        // - date
-        vals[3] = data.attribute(3).parseDate("2001-11-09");
-        // - relational
-        dataRel = new Instances(data.attribute(4).relation(), 0);
-        // -- first instance
-        valsRel = new double[2];
-        valsRel[0] = Math.PI + 1;
-        valsRel[1] = attValsRel.indexOf("val5.3");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        // -- second instance
-        valsRel = new double[2];
-        valsRel[0] = Math.PI + 2;
-        valsRel[1] = attValsRel.indexOf("val5.2");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        vals[4] = data.attribute(4).addRelation(dataRel);
-        // add
-        data.add(new DenseInstance(1.0, vals));
-
-        // second instance
-        vals = new double[data.numAttributes()];  // important: needs NEW array!
-        // - numeric
-        vals[0] = Math.E;
-        // - nominal
-        vals[1] = attVals.indexOf("val1");
-        // - string
-        vals[2] = data.attribute(2).addStringValue("And another one!");
-        // - date
-        vals[3] = data.attribute(3).parseDate("2000-12-01");
-        // - relational
-        dataRel = new Instances(data.attribute(4).relation(), 0);
-        // -- first instance
-        valsRel = new double[2];
-        valsRel[0] = Math.E + 1;
-        valsRel[1] = attValsRel.indexOf("val5.4");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        // -- second instance
-        valsRel = new double[2];
-        valsRel[0] = Math.E + 2;
-        valsRel[1] = attValsRel.indexOf("val5.1");
-        dataRel.add(new DenseInstance(1.0, valsRel));
-        vals[4] = data.attribute(4).addRelation(dataRel);
-        // add
-        data.add(new DenseInstance(1.0, vals));
-
-        // 4. output data
-        System.out.println(data);
-    }
+//    public static void attTest() throws Exception {
+//        ArrayList<Attribute> atts;
+//        ArrayList<Attribute> attsRel;
+//        ArrayList<String>    attVals;
+//        ArrayList<String>    attValsRel;
+//        Instances            data;
+//        Instances            dataRel;
+//        double[]             vals;
+//        double[]             valsRel;
+//        int                  i;
+//
+//        // 1. set up attributes
+//        atts = new ArrayList<Attribute>();
+//        // - numeric
+//        atts.add(new Attribute("att1"));
+//        // - nominal
+//        attVals = new ArrayList<String>();
+//        for (i = 0; i < 5; i++)
+//            attVals.add("val" + (i+1));
+//        atts.add(new Attribute("att2", attVals));
+//        // - string
+//        atts.add(new Attribute("att3", (ArrayList<String>) null));
+//        // - date
+//        atts.add(new Attribute("att4", "yyyy-MM-dd"));
+//        // - relational
+//        attsRel = new ArrayList<Attribute>();
+//        // -- numeric
+//        attsRel.add(new Attribute("att5.1"));
+//        // -- nominal
+//        attValsRel = new ArrayList<String>();
+//        for (i = 0; i < 5; i++)
+//            attValsRel.add("val5." + (i+1));
+//        attsRel.add(new Attribute("att5.2", attValsRel));
+//        dataRel = new Instances("att5", attsRel, 0);
+//        atts.add(new Attribute("att5", dataRel, 0));
+//
+//        // 2. create Instances object
+//        data = new Instances("MyRelation", atts, 0);
+//
+//        // 3. fill with data
+//        // first instance
+//        vals = new double[data.numAttributes()];
+//        // - numeric
+//        vals[0] = Math.PI;
+//        // - nominal
+//        vals[1] = attVals.indexOf("val3");
+//        // - string
+//        vals[2] = data.attribute(2).addStringValue("This is a string!");
+//        // - date
+//        vals[3] = data.attribute(3).parseDate("2001-11-09");
+//        // - relational
+//        dataRel = new Instances(data.attribute(4).relation(), 0);
+//        // -- first instance
+//        valsRel = new double[2];
+//        valsRel[0] = Math.PI + 1;
+//        valsRel[1] = attValsRel.indexOf("val5.3");
+//        dataRel.add(new DenseInstance(1.0, valsRel));
+//        // -- second instance
+//        valsRel = new double[2];
+//        valsRel[0] = Math.PI + 2;
+//        valsRel[1] = attValsRel.indexOf("val5.2");
+//        dataRel.add(new DenseInstance(1.0, valsRel));
+//        vals[4] = data.attribute(4).addRelation(dataRel);
+//        // add
+//        data.add(new DenseInstance(1.0, vals));
+//
+//        // second instance
+//        vals = new double[data.numAttributes()];  // important: needs NEW array!
+//        // - numeric
+//        vals[0] = Math.E;
+//        // - nominal
+//        vals[1] = attVals.indexOf("val1");
+//        // - string
+//        vals[2] = data.attribute(2).addStringValue("And another one!");
+//        // - date
+//        vals[3] = data.attribute(3).parseDate("2000-12-01");
+//        // - relational
+//        dataRel = new Instances(data.attribute(4).relation(), 0);
+//        // -- first instance
+//        valsRel = new double[2];
+//        valsRel[0] = Math.E + 1;
+//        valsRel[1] = attValsRel.indexOf("val5.4");
+//        dataRel.add(new DenseInstance(1.0, valsRel));
+//        // -- second instance
+//        valsRel = new double[2];
+//        valsRel[0] = Math.E + 2;
+//        valsRel[1] = attValsRel.indexOf("val5.1");
+//        dataRel.add(new DenseInstance(1.0, valsRel));
+//        vals[4] = data.attribute(4).addRelation(dataRel);
+//        // add
+//        data.add(new DenseInstance(1.0, vals));
+//
+//        // 4. output data
+//        System.out.println(data);
+//    }
 
 }
 
