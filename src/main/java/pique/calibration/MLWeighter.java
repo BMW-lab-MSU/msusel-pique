b1f7670c507c1c75a6463ebe8ab1001cccbcb789
kaveen <kaveenliyanage@yahoo.com>
2025-03-12 14:43:57 -0600

    MultiPreceptron

--- src/main/java/pique/calibration/MLWeighter.java
+++ src/main/java/pique/calibration/MLWeighter.java
@@ -49,7 +49,7 @@ import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.converters.CSVSaver;
 import weka.classifiers.functions.LinearRegression;
 import weka.classifiers.functions.SimpleLinearRegression;
-import weka.classifiers.functions.Logistic;
+import weka.classifiers.functions.MultilayerPerceptron;
 import weka.filters.Filter;
 import weka.filters.unsupervised.instance.RemovePercentage;
 import weka.filters.supervised.instance.StratifiedRemoveFolds;
@@ -385,6 +385,9 @@ public class MLWeighter implements IWeighter{
             WeightResult weightResult = new WeightResult(node.getName());
             System.out.println(node.getName());
 
+            // Creating Dataset
+            // https://waikato.github.io/weka-wiki/formats_and_processing/creating_arff_file/
+
             Instances data = null;
             try{
                 if (priority.equals("CSV")) {
@@ -668,7 +671,8 @@ public class MLWeighter implements IWeighter{
 
         System.out.println("Cleaned data");
         Instances cleanedData = new Instances(removeConstantAttributes(dataset));
-        System.out.println(cleanedData);
+//        System.out.println(cleanedData);
+        System.out.println(cleanedData.size());
 
 
 //        DataSource source = new DataSource("/PWD/weka-3-8-6/data/regression-datasets/regression-datasets/housing.arff");
@@ -727,8 +731,9 @@ public class MLWeighter implements IWeighter{
 
         //Build model
 //        SimpleLinearRegression model = new SimpleLinearRegression();
-        LinearRegression model = new LinearRegression();
-//        Logistic model = new Logistic();
+//        LinearRegression model = new LinearRegression();
+        MultilayerPerceptron model = new MultilayerPerceptron();
+
         model.buildClassifier(train);
         //output model
         System.out.println("LR FORMULA : "+model);
@@ -755,102 +760,6 @@ public class MLWeighter implements IWeighter{
 
         return (Classifier) model;
     }
-
-//    public static void attTest() throws Exception {
-//        ArrayList<Attribute> atts;
-//        ArrayList<Attribute> attsRel;
-//        ArrayList<String>    attVals;
-//        ArrayList<String>    attValsRel;
-//        Instances            data;
-//        Instances            dataRel;
-//        double[]             vals;
-//        double[]             valsRel;
-//        int                  i;
-//
-//        // 1. set up attributes
-//        atts = new ArrayList<Attribute>();
-//        // - numeric
-//        atts.add(new Attribute("att1"));
-//        // - nominal
-//        attVals = new ArrayList<String>();
-//        for (i = 0; i < 5; i++)
-//            attVals.add("val" + (i+1));
-//        atts.add(new Attribute("att2", attVals));
-//        // - string
-//        atts.add(new Attribute("att3", (ArrayList<String>) null));
-//        // - date
-//        atts.add(new Attribute("att4", "yyyy-MM-dd"));
-//        // - relational
-//        attsRel = new ArrayList<Attribute>();
-//        // -- numeric
-//        attsRel.add(new Attribute("att5.1"));
-//        // -- nominal
-//        attValsRel = new ArrayList<String>();
-//        for (i = 0; i < 5; i++)
-//            attValsRel.add("val5." + (i+1));
-//        attsRel.add(new Attribute("att5.2", attValsRel));
-//        dataRel = new Instances("att5", attsRel, 0);
-//        atts.add(new Attribute("att5", dataRel, 0));
-//
-//        // 2. create Instances object
-//        data = new Instances("MyRelation", atts, 0);
-//
-//        // 3. fill with data
-//        // first instance
-//        vals = new double[data.numAttributes()];
-//        // - numeric
-//        vals[0] = Math.PI;
-//        // - nominal
-//        vals[1] = attVals.indexOf("val3");
-//        // - string
-//        vals[2] = data.attribute(2).addStringValue("This is a string!");
-//        // - date
-//        vals[3] = data.attribute(3).parseDate("2001-11-09");
-//        // - relational
-//        dataRel = new Instances(data.attribute(4).relation(), 0);
-//        // -- first instance
-//        valsRel = new double[2];
-//        valsRel[0] = Math.PI + 1;
-//        valsRel[1] = attValsRel.indexOf("val5.3");
-//        dataRel.add(new DenseInstance(1.0, valsRel));
-//        // -- second instance
-//        valsRel = new double[2];
-//        valsRel[0] = Math.PI + 2;
-//        valsRel[1] = attValsRel.indexOf("val5.2");
-//        dataRel.add(new DenseInstance(1.0, valsRel));
-//        vals[4] = data.attribute(4).addRelation(dataRel);
-//        // add
-//        data.add(new DenseInstance(1.0, vals));
-//
-//        // second instance
-//        vals = new double[data.numAttributes()];  // important: needs NEW array!
-//        // - numeric
-//        vals[0] = Math.E;
-//        // - nominal
-//        vals[1] = attVals.indexOf("val1");
-//        // - string
-//        vals[2] = data.attribute(2).addStringValue("And another one!");
-//        // - date
-//        vals[3] = data.attribute(3).parseDate("2000-12-01");
-//        // - relational
-//        dataRel = new Instances(data.attribute(4).relation(), 0);
-//        // -- first instance
-//        valsRel = new double[2];
-//        valsRel[0] = Math.E + 1;
-//        valsRel[1] = attValsRel.indexOf("val5.4");
-//        dataRel.add(new DenseInstance(1.0, valsRel));
-//        // -- second instance
-//        valsRel = new double[2];
-//        valsRel[0] = Math.E + 2;
-//        valsRel[1] = attValsRel.indexOf("val5.1");
-//        dataRel.add(new DenseInstance(1.0, valsRel));
-//        vals[4] = data.attribute(4).addRelation(dataRel);
-//        // add
-//        data.add(new DenseInstance(1.0, vals));
-//
-//        // 4. output data
-//        System.out.println(data);
-//    }
 
 }
 
